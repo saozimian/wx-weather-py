7019c8b3b242757a7c5f139f04b77cb03f6fae8f
u4250 <48275726+u4250@users.noreply.github.com>
2022-08-25 17:05:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,15 +23,9 @@ template_id = os.environ["TEMPLATE_ID"]
 
 # 用户列表 也可通过接口获取，但是接口获取的只有用户id没有用户昵称，不方便部分数据展示，如果有新增人员，对应添加一个user对象即可
 user_id_list = [
-    {'user_id': 'olqaK5sXw4RM4PEaRgi9I0kH4ldE', "name": 'ㅤ', "date": "2021-03-01", "birthday": "03-26",
+    {'user_id': 'oADCp5pGHQEPlfSLGmUDBWfvSCws', "name": '敏儿', "date": "2021-03-01", "birthday": "03-26",
      'city': '440300'},
      {'user_id': 'olqaK5iLtf1e1O8y50FrAyoVF5G8', "name": "缱绻", "date": "2021-03-01", "birthday": "10-09",
-     'city': '440300'},
-     {'user_id': 'olqaK5nbxrC4ehk7Lf9QkSHHbfng', "name": "四喜丸子", "date": "2021-03-01", "birthday": "02-12",
-     'city': '440300'},
-     {'user_id': 'olqaK5p-v3b6gvFif4XLaAwfg0EE', "name": "别南山", "date": "2021-03-01", "birthday": "10-21",
-     'city': '440300'},
-     {'user_id': 'olqaK5haaWE7C8NJ8bclDpKp0Zg8', "name": "苏子瑜", "date": "2021-03-01", "birthday": "05-01",
      'city': '440300'}
 ]
 
